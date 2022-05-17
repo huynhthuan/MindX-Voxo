41cac5c59fb2251bb4d13f051ae5a9a2102a5b57
DoMinhHieu1997 <36026729+DoMinhHieu1997@users.noreply.github.com>
2022-05-17 15:04:28 +0700

    add api get comment

--- utils/api_minhhieu/index.js
+++ utils/api_minhhieu/index.js
@@ -6,5 +6,6 @@ const FEATURED_MEDIA = BASE_URL_API + '/wp-json/wp/v2/media/';
 const POST_AUTHOR = BASE_URL_API + '/wp-json/wp/v2/users/';
 const POST_COMMENT_LIST = BASE_URL_API + '/wp-json/wp/v2/comments?post=';
 const COMMENT = BASE_URL_API + '/wp-json/wp/v2/comments/';
+const COMMENT_LIST_BY_POST = BASE_URL_API + '/wp-json/wp/v2/comments?post=';
 
 export { BLOG_LIST, BlOG_LIST_CATEGORY, FEATURED_MEDIA, POST_AUTHOR, POST_COMMENT_LIST, COMMENT };
