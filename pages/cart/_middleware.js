f08194782cb832d44916274a8ed6e7bbb329cd3f
huynhthuan <huynhthuanvugia@gmail.com>
2022-07-16 15:52:37 +0700

    Fix cookie

--- pages/cart/_middleware.js
+++ pages/cart/_middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
+    console.log(req.cookies.wordpress_login);
     if (req.cookies.wordpress_login) {
         return NextResponse.next();
     } else {
